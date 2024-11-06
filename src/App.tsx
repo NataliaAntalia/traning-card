3da7ea0b918889ddc94e2ad775b655c532ee9104
Natalia <nataliaantaliaa@gmail.com>
2024-11-06 13:22:55 +0200

    fixed other files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import {StyledBackground} from "./components/StyledBackground";
+import {StyledButton, SuperButton} from "./components/StyledButton";
+import {StyledText, Text} from "./components/StyledText";
+import imgS from "./images/image.webp"
+import {StyledImage} from "./components/StyledImage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <StyledBox>
+
+        <StyledBackground>
+          <StyledImage src={imgS} alt="photo portfolio card"></StyledImage>
+          <Text>Headline</Text>
+          <StyledText>Faucibus. Faucibus. Sit sit sapien sit
+            tempusrisu ut. Sit molestie ornare in venen.</StyledText>
+
+          <StyledButton>See more</StyledButton>
+
+          <SuperButton>Save</SuperButton>
+
+        </StyledBackground>
+
+
+      </StyledBox>
   );
 }
+
+
+const StyledBox = styled.div`
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+
+    @media screen and (max-width: 600px) {
+        flex-direction: revert;
+        flex: 1 1 100%; 
+    }
+`
+
 
 export default App;
